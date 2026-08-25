fac5ee9f08893be908dfa479b5c62a2e2a0d691f
Indianajonas235 <jonasbernert@icloud.com>
2026-08-25 17:38:16 +0200

    Transform bugfixes and file creation Fixed some of the bugs within the transform program and added the pixels.json file

--- python/transform.py
+++ python/transform.py
@@ -10,8 +10,8 @@ def transform(x, y, m = 0):
     return t
 
 def get_intersection_point(m0, m1, x1, y1, x2, y2):
-    l1 = Line(Point(m0, transform(x1, y1, m0)), Point(m1, transform(x1, x2, m1)))
-    l2 = Line(Point(m0, transform(x2, y2, m0)), Point(m1, transform(x1, x2, m1)))
+    l1 = Line(Point(m0, transform(x1, y1, m0)), Point(m1, transform(x1, y1, m1)))
+    l2 = Line(Point(m0, transform(x2, y2, m0)), Point(m1, transform(x2, y2, m1)))
     intersection_point = l1.intersection(l2)
     return intersection_point
 
@@ -24,13 +24,16 @@ def intersect_available_lines(m0, m1, file_name = "pixels.json"):
         for point1, point2 in itertools.combinations(pixels, 2):
             x1, y1 = point1
             x2, y2 = point2
-            Intersects.append(get_intersection_point(m0, m1, x1, y1, x2, y2))
-            print(get_intersection_point(m0, m1, x1, y1, x2, y2)) # UNFINISHED
+            Intersects.extend(get_intersection_point(m0, m1, x1, y1, x2, y2))
+            #print(get_intersection_point(m0, m1, x1, y1, x2, y2))
+        #print(Intersects)
 
-def find_most_common_point():
-    intersect_available_lines()
-    counter = Counter(Intersects)
+def find_most_common_point(m0 = 0, mmax = 1):
+    intersect_available_lines(m0, mmax)
+    flat_intersects = [p for sublist in Intersects for p in sublist]
+    counter = Counter(flat_intersects)
     most_common_intersect = counter.most_common(1)[0][0]
     return most_common_intersect
 
-intersect_available_lines(0, 1)
+#print("Intersect: ", find_most_common_point())
+#intersect_available_lines(0, 1)
